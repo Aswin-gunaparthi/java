724c06cd85559a8fbdd83543e00dd09f2b8bb76b
Aswin <6300639674a@gmail.com>
2025-11-22 18:55:42 +0530

    ntg

---
+++ src/arithmetic_operators.java
@@ -0,0 +1,29 @@
+public class arithmetic_operators {
+    public static void main(String[] args) {
+
+        double x = 10;
+        double y = 20;
+        double result;
+
+//        result = x + y;
+//        result = x - y;
+//        result = x * y;
+//        result = x / y;
+
+//        x += y;
+//        x -= y;
+//        x *= y;
+//        x /= y;
+        result = x % y;
+
+        //x += 1;
+
+        //order of operations [p-e-m-d-a-s]
+
+
+
+
+
+
+    }
+}
