1fc8cefa9550df54a80542d6dc0446ebcd41ec2e
Aswin_Gunaparthi <167799272+Aswin-gunaparthi@users.noreply.github.com>
2025-11-22 16:47:33 +0530

    Add files via upload

---
+++ area_of_rectangle.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class area_of_rectangle {
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        double length = 0;
+        double breath = 0;
+        double area = 0;
+
+        System.out.print("enter the length of the rectangle: ");
+        length = scanner.nextDouble();
+
+        System.out.print("enter breath: ");
+        breath = scanner.nextDouble();
+
+        area = length * breath;
+
+        System.out.print("the area of the rectangle is: " + area + "cm²");
+
+        scanner.close();
+
+    }
+}
