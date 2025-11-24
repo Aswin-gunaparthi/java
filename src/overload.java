2b730f5bee3902ac02af1a5b6f77eaa369616bf9
Aswin <6300639674a@gmail.com>
2025-11-24 20:56:48 +0530

    overload methods

---
+++ src/overload.java
@@ -0,0 +1,22 @@
+public class overload {
+    public static void main(String[] args) {
+
+        String pizza = bakePizza("flat bread","mozzarella","pepperoni");
+
+        System.out.println(pizza);
+    }
+
+    static  String bakePizza(String bread){
+        return bread + " pizza";
+    }
+
+    static  String bakePizza(String bread, String cheese){
+        return cheese + " " + bread + " pizza";
+    }
+
+    static  String bakePizza(String bread, String cheese, String toppings){
+        return toppings + " " + cheese + " " + bread + " pizza";
+    }
+
+
+}
