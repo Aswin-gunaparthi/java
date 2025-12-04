ba9acabe5b71ef56a5cea2c5f4cacd774ce7a685
Aswin <6300639674a@gmail.com>
2025-12-04 22:25:06 +0530

    collage code(incomplete)

---
+++ src/javaLab/binarySearch.java
@@ -0,0 +1,19 @@
+package javaLab;
+
+import java.util.Scanner;
+
+public class binarySearch {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int i;
+
+        System.out.println("Enter the size of list: ");
+        i = scanner.nextInt();
+
+        int[] list = new int[i];
+
+
+        scanner.close();
+    }
+}
