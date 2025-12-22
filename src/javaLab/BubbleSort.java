06cab9fe08a60f30988d0f6e2eadd5c3e6458bce
Aswin_Gunaparthi <167799272+Aswin-gunaparthi@users.noreply.github.com>
2025-12-22 15:00:20 +0530

    Create BubbleSort.java

---
+++ src/javaLab/BubbleSort.java
@@ -0,0 +1,33 @@
+package javaLab;
+import java.util.Arrays;
+import java.util.Scanner;
+
+// Write a JAVA program to sort for an element in a given list of elements using bubble sort
+
+public class BubbleSort{
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        int size;
+        
+        System.out.print("Enter size of the list: ");
+        size = scanner.nextInt();
+        int[] nums = new int[size];
+
+        for (int i = 0; i < size; i++){
+            System.err.print("enter " + (i + 1) + " element: ");
+            nums[i] = scanner.nextInt();
+        }
+
+        Arrays.sort(nums);
+
+        System.err.println("The sorted list is: ");
+
+        for (int num : nums) {
+            System.err.println(num);
+        }
+        
+        scanner.close();
+    }
+}
